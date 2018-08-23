54aaba980503f001e3d41d372610a0e6ebb592d9
Tristan Kraft <tristankraft@gmail.com>
2018-08-23 20:26:03 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -65,10 +65,10 @@ function sayHiToGrandma(string) {
 if (string === "I love you, Grandma.") {
 return "I love you, too."}
 
-if (string.toLowercase === string) {
+if (string.toLowerCase() === string) {
 return "I can\'t hear you!" }
 
-if (string.toUppercase === string) {
+if (string.toUpperCase() === string) {
 return "YES INDEED!"}
 }
 
